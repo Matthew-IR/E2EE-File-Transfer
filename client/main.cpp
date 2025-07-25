ae6604a01d6e4f0321319385c9b9f348a037d9a0
Matthew-IR <67317204+Matthew-IR@users.noreply.github.com>
2025-07-25 20:32:25 +0100

    client error handling

--- client/main.cpp
+++ client/main.cpp
@@ -7,10 +7,24 @@
 
 int main(int argc, char const* argv[]) {
 
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <hostname>\n" << std::endl;
+        exit(EXIT_FAILURE);
+    };
+
     const char* server_hostname = argv[1];
     struct hostent *server = gethostbyname(server_hostname);
 
+    if (server == NULL) {
+        std::cout << "No host: " << server_hostname << std::endl;
+        exit(EXIT_FAILURE);
+    };
+
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == -1) {
+        std::cerr << "socket error" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
@@ -18,10 +32,21 @@ int main(int argc, char const* argv[]) {
 
     memcpy(&serverAddress.sin_addr.s_addr, server->h_addr, server->h_length);
 
-    connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+        std::cerr << "connection error" << std::endl;
+        close(clientSocket);
+        exit(EXIT_FAILURE);
+    }
+
+    std::cerr << "Connected to: " << server_hostname << std::endl;
 
     const char* message = "Hello, server!";
-    send(clientSocket, message, strlen(message), 0);
+
+    if(send(clientSocket, message, strlen(message), 0) < 0) {
+        std::cerr << "error sending message" << std::endl;
+    } else {
+        std::cerr << "message sent: " << message << std::endl;
+    };
 
     close(clientSocket);
 
