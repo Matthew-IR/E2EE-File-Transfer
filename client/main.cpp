349a96d011c0d9898ee99191932eebb4ce83bd16
Matthew-IR <67317204+Matthew-IR@users.noreply.github.com>
2025-07-25 20:06:32 +0100

    Initialise TCP/IP connection

--- client/main.cpp
+++ client/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+#include <netdb.h> 
 
-int main() {
-    std::cout << "Hello from client" << std::endl;
+int main(int argc, char const* argv[]) {
+
+    const char* server_hostname = argv[1];
+    struct hostent *server = gethostbyname(server_hostname);
+
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+
+    memcpy(&serverAddress.sin_addr.s_addr, server->h_addr, server->h_length);
+
+    connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    const char* message = "Hello, server!";
+    send(clientSocket, message, strlen(message), 0);
+
+    close(clientSocket);
+
     return 0;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string>
+
 
 int main() {
-    std::cout << "Hello from server" << std::endl;
+
+    // Create a socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Define the server address
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    
+    // Bind the socket to IP and port
+    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    // Listen for incoming connections
+    listen(serverSocket, 5);
+
+    // Accept a connection
+    int clientSocket = accept(serverSocket, nullptr, nullptr);
+
+    //Receive and Send data
+    char buffer[1024] = {0};
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+    std::cout << "Message from client: " << buffer << std::endl;
+
+    // Close the sockets
+
+    close(serverSocket);
+    close(clientSocket);
     return 0;
 }
