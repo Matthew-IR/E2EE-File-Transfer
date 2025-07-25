e8b10cfb2e8f73de7d0b4df175b95c0787e27760
Matthew-IR <67317204+Matthew-IR@users.noreply.github.com>
2025-07-25 20:22:39 +0100

    server error handling

--- server/main.cpp
+++ server/main.cpp
@@ -9,6 +9,10 @@ int main() {
 
     // Create a socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        std::cerr << "socket error" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
     // Define the server address
     sockaddr_in serverAddress;
@@ -17,18 +21,39 @@ int main() {
     serverAddress.sin_addr.s_addr = INADDR_ANY;
     
     // Bind the socket to IP and port
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+        std::cerr << "bind error" << std::endl;
+        close(serverSocket);
+        exit(EXIT_FAILURE);
+    }
 
     // Listen for incoming connections
-    listen(serverSocket, 5);
+    if (listen(serverSocket, 5) == -1) {
+        std::cerr << "listen failed" << std::endl;
+        close(serverSocket);
+        exit(EXIT_FAILURE);
+    };
 
     // Accept a connection
     int clientSocket = accept(serverSocket, nullptr, nullptr);
+    if (clientSocket == -1) {
+        std::cerr << "accept error" << std::endl;
+        close(serverSocket);
+        exit(EXIT_FAILURE);
+    }
 
     //Receive and Send data
     char buffer[1024] = {0};
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    std::cout << "Message from client: " << buffer << std::endl;
+    ssize_t bytes = recv(clientSocket, buffer, sizeof(buffer), 0);
+
+    if (bytes == -1) {
+        std::cout << "recv failed " << std::endl;
+    } else if (bytes == 0) {
+        std::cout << "client disconnect" << std::endl;
+    } else {
+        std::cout << "Message from client: " << buffer << std::endl;
+    }
+    
 
     // Close the sockets
 
